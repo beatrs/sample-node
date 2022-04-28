5fe2a483cc12b64797ebee1aca48a7251c18a3e6
Bea <201811991@feualabang.edu.ph>
2022-04-28 14:12:54 +0800

    fix

--- db.js
+++ db.js
@@ -19,7 +19,7 @@ const pool = new Pool({
     }
 });
 
-pool.connect();
+
 
 
 // export file
--- index.js
+++ index.js
@@ -24,6 +24,19 @@ if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 // });
 
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 app.get('/test', (req, res) => {
     res.json({test: "hello world"});
 })
